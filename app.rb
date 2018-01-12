f3f2cc5f3057ad0bca0221d97a2d0f35fe001473
Sherry El Garhy <sherryptk@gmail.com>
2018-01-12 23:45:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+  @translated_text = PigLatinizer.new(params[:user_phrase])
+
+  erb :results
+  end
+
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,106 @@
+class PigLatinizer
+
+  def intialize
+  end
+
+  def piglatinize(sent)
+    vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    words = sent.split(' ')
+    result = []
+
+  words.each_with_index do |word, i|
+      translation = ''
+      qu = false
+
+      if vowels.include? word[0]
+          translation = word + 'way'
+          result.push(translation)
+
+      elsif
+          word = word.split('')
+          count = 0
+          word.each_with_index do |char, index|
+              if vowels.include? char
+                  # handle words that start with 'qu'
+                  if char == 'u' and translation[-1] == 'q'
+                      qu = true
+                      translation = words[i][count + 1..words[i].length] + translation + 'uay'
+                      result.push(translation)
+                      next
+                  end
+                  break
+              else
+                  # handle words with 'qu' in middle
+                  if char == 'q' and word[i+1] == 'u'
+                      qu = true
+                      translation = words[i][count + 2..words[i].length] + 'quay'
+                      result.push(translation)
+                      next
+                  else
+                      translation += char
+                  end
+                  count += 1
+              end
+          end
+          # translation of consonant words without qu
+          if not qu
+              translation = words[i][count..words[i].length] + translation + 'ay'
+              result.push(translation)
+          end
+      end
+
+  end
+  result.join(' ')
+  end
+
+  def to_pig_latin(sent)
+    vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    words = sent.split(' ')
+    result = []
+
+    words.each_with_index do |word, i|
+      translation = ''
+      qu = false
+
+      if vowels.include? word[0]
+          translation = word + 'way'
+          result.push(translation)
+
+      elsif
+          word = word.split('')
+          count = 0
+          word.each_with_index do |char, index|
+              if vowels.include? char
+                  # handle words that start with 'qu'
+                  if char == 'u' and translation[-1] == 'q'
+                      qu = true
+                      translation = words[i][count + 1..words[i].length] + translation + 'uay'
+                      result.push(translation)
+                      next
+                  end
+                  break
+              else
+                  # handle words with 'qu' in middle
+                  if char == 'q' and word[i+1] == 'u'
+                      qu = true
+                      translation = words[i][count + 2..words[i].length] + 'quay'
+                      result.push(translation)
+                      next
+                  else
+                      translation += char
+                  end
+                  count += 1
+              end
+          end
+          # translation of consonant words without qu
+          if not qu
+              translation = words[i][count..words[i].length] + translation + 'ay'
+              result.push(translation)
+          end
+      end
+
+  end
+  result.join(' ')
+  end
+
+end
